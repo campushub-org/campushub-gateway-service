8b159c483280c4108b67e3e13d19c29dc68ffc5b
JordanFotso <achillefotso43@gmail.com>
2025-12-10 20:46:52 +0100

    fix:core protection

--- src/main/java/campushub/campushub_gateway_service/GatewayConfig.java
+++ src/main/java/campushub/campushub_gateway_service/GatewayConfig.java
@@ -4,6 +4,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class GatewayConfig {
@@ -24,5 +29,19 @@ public class GatewayConfig {
                     .filters(f -> f.rewritePath("/campushub-notification-service/(?<remaining>.*)", "/${remaining}"))
                     .uri("lb://campushub-notification-service"))
             .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Arrays.asList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
